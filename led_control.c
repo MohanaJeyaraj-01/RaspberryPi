0933e4b001a12964eaa12de343b1a54c6ebefca6
Supriya D <supriyaad21@gmail.com>
2025-04-01 20:13:17 +0530

    led_control.c

--- led_control.c
+++ led_control.c
@@ -1,22 +1,22 @@
-// Define pin numbers
-const int LED_PIN = 13;      // Pin connected to the LED
-const int BUTTON_PIN = 2;    // Pin connected to the button
+//pins
+const int LED_PIN = 13;      
+const int BUTTON_PIN = 2;    
 
 void setup() {
-  // Set up the LED pin as an OUTPUT
+  // led for O/P
   pinMode(LED_PIN, OUTPUT);
   
-  // Set up the button pin as an INPUT
-  pinMode(BUTTON_PIN, INPUT_PULLUP);  // Using internal pull-up resistor
+  // button for I/P
+  pinMode(BUTTON_PIN, INPUT_PULLUP);  
 }
 
 void loop() {
-  // Read the button state (LOW when pressed)
+  // Reading btn state
   int buttonState = digitalRead(BUTTON_PIN);
   
   if (buttonState == LOW) {
-    digitalWrite(LED_PIN, HIGH);  // Turn LED ON when button is pressed
+    digitalWrite(LED_PIN, HIGH);  // LED ON when button is pressed
   } else {
-    digitalWrite(LED_PIN, LOW);   // Turn LED OFF when button is not pressed
+    digitalWrite(LED_PIN, LOW);   // LED OFF when button is not pressed
   }
 }
